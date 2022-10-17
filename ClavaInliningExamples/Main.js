0903302487df430fc0971c7e3a2264d8cc48227a
João Bispo <joaobispo@gmail.com>
2022-10-17 15:11:31 +0100

    Adds working inlining example

--- ClavaInliningExamples/Main.js
+++ ClavaInliningExamples/Main.js
@@ -1,12 +1,26 @@
 "use strict";
 
+laraImport("clava.opt.NormalizeToSubset");
+laraImport("clava.code.Inliner");
+
 laraImport("weaver.WeaverJps");
+laraImport("weaver.Query");
 
-const funs = [];
-for (var elem of WeaverJps.search("function")) {
-    if (elem.isImplementation) {
-        funs.push(elem);
-    }
+// Normalize all code
+NormalizeToSubset(Query.root());
+
+// Inline calls in main
+for (const $call of Query.search("function", "main").search("call")) {
+  const inliner = new Inliner();
+
+  // inline() accepts an exprStmt. All calls must be inside exprStmt now
+  const $callParent = $call.ancestor("exprStmt");
+  if (!$callParent.instanceOf("exprStmt")) {
+    println(
+      `Could not inline call ${$call.name}@${$call.location}, ancestor is ${$callParent.joinPointType}`
+    );
+    continue;
+  }
+
+  inliner.inline($callParent);
 }
-
-println(funs);
