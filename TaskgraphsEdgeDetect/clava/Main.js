2678e82e684fc8a868739ac668125a0e0026eedf
tiagolascasas <tiagolascasas@hotmail.com>
2022-11-22 03:40:53 +0000

    changed folder names

---
+++ TaskgraphsEdgeDetect/clava/Main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+laraImport("clava.ClavaJoinPoints");
+laraImport("weaver.WeaverJps");
+laraImport("lara.vitishls.VitisHls");
+
+const files = {
+	"edge_detect_tasks_V0.c": ["rgbToGrayscale", "convolve2d", "combthreshold"],
+	"edge_detect_tasks_V0_const.c": ["rgbToGrayscale", "convolve2d_smooth", "convolve2d_vert", "convolve2d_horiz", "combthreshold"],
+	"edge_detect_tasks_V1.c": ["rgbToGrayscale", "convolve2d", "horizontal_and_vertical_filtering_task", "combthreshold"],
+	"edge_detect_tasks_V1_const.c": ["rgbToGrayscale", "convolve2d_smooth", "horizontal_and_vertical_filtering_task", "combthreshold"],
+	"edge_detect_tasks_V2.c": ["rgbToGrayscale", "convolve2d_smooth", "convolve2d_parallel", "combthreshold"],
+	"edge_detect_tasks_V2_const.c": ["rgbToGrayscale", "convolve2d_smooth", "convolve2d_vert_horiz", "combthreshold"]
+}
+
+var results = "";
+
+for (const file in files) {
+	println(file);
+	const funs = files[file];
+
+	for (const fun of funs) {
+		println(fun);
+
+		var vitis = new VitisHls(fun, 5, "xcvc1902-vsvd1760-2MP-e-S");
+		vitis.addSource(file);
+		vitis.setFlowTarget("vitis");
+		var success = vitis.synthesize();
+		
+		if (success) {
+		   var report = vitis.getSynthesisReport();
+		   vitis.prettyPrintReport(report);
+		   results += `${file},${fun},${report["execTimeWorst"]},${report["latencyWorst"]},${report["fmax"]}\n`;
+		}
+		println(results);
+	}
+	results += "\n";
+}
+
+
---
+++ TaskgraphsEdgeDetect/src/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define ITER 1000
+
+#define K 3
+#define N 512 // 128
+#define T 80  // 127
+
+#define H N
+#define W N
