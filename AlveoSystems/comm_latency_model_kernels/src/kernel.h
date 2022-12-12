4e82b3599a73526d4773d333133d37a0809674ba
tiagolascasas <tiagolascasas@hotmail.com>
2022-12-12 14:45:31 +0000

    comm model new

--- AlveoSystems/comm_latency_model_kernels/src/kernel.h
+++ AlveoSystems/comm_latency_model_kernels/src/kernel.h
@@ -1,4 +1,10 @@
 #pragma once
 
-template<unsigned int t_size>
-void kernel_foo(int X[t_size], int *res);
+//template<unsigned int ARRAY_SIZE>
+//void kernel_foo(int X[ARRAY_SIZE], int *res);
+
+#define N1 1000
+#define N2 100000
+#define N3 500000
+#define N4 1000000
+#define N5 5000000
