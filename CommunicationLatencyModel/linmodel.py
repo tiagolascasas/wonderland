1f3ea653a8314dfaead7c87483f4f0a14f0d5240
tiagolascasas <tiagolascasas@outlook.com>
2023-01-05 02:46:20 +0000

    Double plot for comm model

--- CommunicationLatencyModel/linmodel.py
+++ CommunicationLatencyModel/linmodel.py
@@ -2,18 +2,35 @@ import numpy as np
 import csv
 from io import StringIO
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 
 def main():
     model_name = "CommLatency"
 
     csv1 = "Int_Roundtrip.csv"
-    m1, b1 = linreg(csv1)
+    m1, b1, x1, y1 = linreg(csv1)
     modelToJS(m1, b1, model_name + "RoundTrip")
 
     csv2 = "Int_Oneway.csv"
-    m2, b2 = linreg(csv2)
+    m2, b2, x2, y2 = linreg(csv2)
     modelToJS(m2, b2, model_name + "OneWay")
+
+    plt.scatter(x1, y1, color="black")
+    x1_line = np.linspace(0, 5000000, 5000000)
+    y1_line = m1 * x1_line + b1
+    plt.plot(x1_line, y1_line, color="red", label="CPU -> FPGA -> CPU")
+
+    plt.scatter(x2, y2, color="grey")
+    x2_line = np.linspace(0, 5000000, 5000000)
+    y2_line = m2 * x2_line + b2
+    plt.plot(x2_line, y2_line, color="blue", label="CPU -> FPGA")
+
+    plt.legend(loc='upper left')
+    plt.xlabel("Number of words (4 bytes) communicated to kernel")
+    plt.ylabel("Communication time (us)")
+    plt.ticklabel_format(style='plain')
+    plt.savefig('comm_model.pdf')
 
 
 def linreg(csv_name):
@@ -40,7 +57,7 @@ def linreg(csv_name):
         f"Adjusted R^2: {1 - (1-model.score(x, y))*(len(y)-1)/(len(y)-x.shape[1]-1)}")
     print(f"intercept: {b}")
     print(f"slope: {m}")
-    return m, b
+    return m, b, x_raw, y_raw
 
 # for i in range(0, len(x)):
 #    print(f"{x[i][0]} -> {int(x[i][0] * m + b)} (actual: {y[i]})")
