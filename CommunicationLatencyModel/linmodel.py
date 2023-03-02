736225c58ef2f932595b4ee1d81e719b9164fdbc
tiagolascasas <tiagolascasas@outlook.com>
2023-03-02 23:52:51 +0000

    Bunch of stuff

--- CommunicationLatencyModel/linmodel.py
+++ CommunicationLatencyModel/linmodel.py
@@ -3,6 +3,7 @@ import numpy as np
 import csv
 from io import StringIO
 from sklearn.linear_model import LinearRegression
+from matplotlib import rc
 import matplotlib.pyplot as plt
 
 
@@ -17,21 +18,24 @@ def main():
     m2, b2, x2, y2 = linreg(csv2)
     modelToJS(m2, b2, model_name + "OneWay")
 
+    rc('font', **{'family': 'serif', 'serif': ['Helvetica']})
+    rc('text', usetex=True)
+
     plt.scatter(x1, y1, color="black")
     x1_line = np.linspace(0, 5000000, 5000000)
     y1_line = m1 * x1_line + b1
-    plt.plot(x1_line, y1_line, color="royalblue", label="CPU -> FPGA -> CPU")
+    plt.plot(x1_line, y1_line, color="royalblue", label="CPU → FPGA → CPU")
 
     plt.scatter(x2, y2, color="grey")
     x2_line = np.linspace(0, 5000000, 5000000)
     y2_line = m2 * x2_line + b2
-    plt.plot(x2_line, y2_line, color="darkorange", label="CPU -> FPGA")
+    plt.plot(x2_line, y2_line, color="darkorange", label="CPU → FPGA")
 
     m3 = m1 - m2
     b3 = -b2 + b1
     x3_line = np.linspace(0, 5000000, 5000000)
     y3_line = m3 * x3_line + b3
-    plt.plot(x3_line, y3_line, color="mediumseagreen", label="FPGA -> CPU")
+    plt.plot(x3_line, y3_line, color="mediumseagreen", label="FPGA → CPU")
 
     plt.xlim(left=0)
     plt.ylim(bottom=0)
@@ -39,7 +43,7 @@ def main():
     plt.xlabel("Number of words (4 bytes) communicated to kernel")
     plt.ylabel("Communication time (us)")
     plt.ticklabel_format(style='plain')
-    plt.savefig('CommModel.pdf')
+    plt.savefig('CommModel.pdf', bbox_inches='tight')
 
 
 def linreg(csv_name):
---
+++ CommunicationLatencyModel/run.cmd
@@ -0,0 +1,1 @@
+python linmodel.py Int_Roundtrip.csv Int_Oneway.csv
