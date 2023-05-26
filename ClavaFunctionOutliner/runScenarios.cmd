861e5cc912e3f2aff5917f3d5c2160aa7b8cfed7
tiagolascasas <tiagolascasas@outlook.com>
2023-05-26 15:14:35 +0100

    Small changes to outliner ID generatioN

--- ClavaFunctionOutliner/runScenarios.cmd
+++ ClavaFunctionOutliner/runScenarios.cmd
@@ -1,1 +1,3 @@
-clava TestOutliner.js -pi -par -std c11 -cr -cl -cs -s -cfs -p input_scenarios -b 0
+cd src
+clava ../TestOutliner.js -pi -par -std c11 -cr -cl -cs -s -cfs -p ../input_scenarios -b 0 -of ../woven_code
+cd ..
--- ClavaFunctionOutliner/runScenarios.sh
+++ ClavaFunctionOutliner/runScenarios.sh
@@ -1,1 +1,2 @@
-clava TestOutliner.js -pi -par -std c11 -cr -cl -cs -s -cfs -p input_scenarios -b 0
+cd src
+clava ../TestOutliner.js -pi -par -std c11 -cr -cl -cs -s -cfs -p ../input_scenarios -b 0
