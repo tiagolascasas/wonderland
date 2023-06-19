1fb006ad724e567ded19592d623399735b18f4d0
tiagolascasas <tiagolascasas@hotmail.com>
2023-06-19 16:07:55 +0100

    Some boilerplate for the constant propagator

--- Clava/ConstantPropagator/src/clava/code/ConstantPropagator.js
+++ Clava/ConstantPropagator/src/clava/code/ConstantPropagator.js
@@ -6,10 +6,42 @@ class ConstantPropagator {
     constructor() { }
 
     doPassesUntilStop(maxPasses = 99) {
+        let passes = 1;
+        let hasChanged = this.doPass();
+
+        while (hasChanged && passes < maxPasses) {
+            hasChanged = this.doPass();
+            passes++;
+        }
+        return passes;
     }
 
     doPass() {
         println("Propagating");
+
+        for (const fun of Query.search("function")) {
+            this.#propagateInFunction(fun);
+        }
+
         return false;
+    }
+
+    #propagateInFunction(fun) {
+        const allDecls = Query.searchFrom(fun, "vardecl").chain();
+        const allParams = Query.searchFrom(fun, "param").chain();
+        const allDefs = [...allDecls, ...allParams];
+
+        for (const def of allDefs) {
+            const refChain = this.#findRefChain(def, fun);
+            this.#propagateChain(refChain);
+        }
+    }
+
+    #findRefChain(def, fun) {
+        return [];
+    }
+
+    #propagateChain(refChain) {
+        println(refChain.length);
     }
 }
