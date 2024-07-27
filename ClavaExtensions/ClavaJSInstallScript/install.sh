560ace2f4f5a1a0eb1b072aafad49765a51fdca1
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-07-27 02:13:53 +0100

    Updates Clava-JS install script

--- ClavaExtensions/ClavaJSInstallScript/install.sh
+++ ClavaExtensions/ClavaJSInstallScript/install.sh
@@ -1,18 +1,19 @@
 CLAVAJS_INSTALL_DIR=$HOME/Clava-JS
 
 # install dependencies
-sudo apt install nodejs npm default-jdk gradle curl git
+sudo apt install nodejs npm openjdk-21-jdk curl git
+# + install gradle 8.9
 
 # ensure node 18 is installed
 if command -v nvm >/dev/null 2>&1; then
     echo "nvm is installed"
 else
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ]
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 fi
 nvm install 18
-node -v
-npm -v
 
 # create install directory from scratch
 rm -rf $CLAVAJS_INSTALL_DIR
@@ -33,6 +34,7 @@ echo "$PACKAGE_JSON" > ./package.json
 # clone repositories and build
 git clone https://github.com/specs-feup/clava
 git clone https://github.com/specs-feup/lara-framework
+git clone https://github.com/specs-feup/specs-java-libs
 cd clava
 git checkout feature/clava-js
 cd ..
@@ -51,8 +53,15 @@ npm list -g --depth=0 | grep clava-js
 
 # build clava weaver
 cd $CLAVAJS_INSTALL_DIR
-git clone https://github.com/specs-feup/specs-java-libs
-git checkout java-updates
 cd clava/ClavaWeaver
 gradle installDist
+cd ../Clava-JS
+ln -s ../ClavaWeaver/build/install/ClavaWeaver java-binaries
+
+# give execute permissions to clava-js
+clava_js_path=$(whereis -b clava-js | awk '{print $2}')
+sudo chmod +x "$clava_js_path"
+
+# verify installation
+clava-js --help
 
