0663c041b64c6dd4b223f77724a298e37d9680b9
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-02-08 03:01:27 +0000

    OmpSs dotproduct example fully compiled

--- OmpSs/run.sh
+++ OmpSs/run.sh
@@ -3,6 +3,7 @@ docker run -it \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /media/:/media/ \
     -v /media/tls/bb85d90f-e2ee-425a-98fb-5dc0f287682c/tools/Xilinx/:/opt/xilinx/ \
+    -v /media/tls/bb85d90f-e2ee-425a-98fb-5dc0f287682c/tools/Xilinx/Xilinx.lic:/home/ompss/Xilinx.lic \
     -v $(pwd)/dockerImageFiles/example_print:/home/ompss/example_print \
 	-e DISPLAY=$DISPLAY \
     -e PATH="/opt/xilinx/Vivado/2024.1/bin/:/opt/xilinx/Vitis_HLS/2024.1/bin/:$PATH" \
