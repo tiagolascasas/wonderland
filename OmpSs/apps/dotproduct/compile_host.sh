5d2a8b367d30adb358af03200f8d47499519931f
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-02-14 17:08:40 +0000

    OmpSs script to compile to AArch64

---
+++ OmpSs/apps/dotproduct/compile_host.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export NANOS6_HOME=/opt/bsc/arm64/ompss-2/3.2.0/nanos6
+export CROSS_COMPILE=aarch64-linux-gnu-
+make dotproduct-p dotproduct-d dotproduct-i
---
+++ OmpSs/apps/dotproduct/compile_kernel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+make bitstream-p BOARD=zcu102
