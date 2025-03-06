edede1bdf2c39cb901e97da6a2bfe3f07c162c57
tiagolascasas <tiagolascasas@outlook.com>
2025-03-06 00:13:19 +0000

    Compilation scripts for all OmpSs apps

--- OmpSs/apps/reverse_print/compile_host.sh
+++ OmpSs/apps/reverse_print/compile_host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NANOS6_HOME=/opt/bsc/arm64/ompss-2/3.2.0/nanos6
 export CROSS_COMPILE=aarch64-linux-gnu-
-make dotproduct-p dotproduct-d dotproduct-i
+make reverse_print-p reverse_print-d reverse_print-i
