55d4b9b36ed545f4b6a6951e23df3c3eb76fecdd
tiagolascasas <tiagolascasas@outlook.com>
2025-06-06 01:39:42 +0100

    Separates Zynq and Kria scripts in different folders

--- AmdVitisAutomation/zynqmp/compile_zynq.sh
+++ AmdVitisAutomation/zynqmp/compile_zynq.sh
@@ -2,10 +2,9 @@
 
 # System defaults, change these if needed
 DEFAULT_VITIS_DIR="/tools/Xilinx/Vitis/2024.2"
-#SYSROOT="/home/tls/xilinx/sysroots/xilinx-zynqmp-common-v2024.2/sysroots/cortexa72-cortexa53-xilinx-linux"
-SYSROOT="/home/tls/xilinx/sysroots/kria-ubuntu-24.04/sysroot"
-#PLATFORM="xilinx_zcu102_base_202420_1"
-PLATFORM="/home/tls/xilinx/platforms/xilinx_kv260_bist_202410_1/kv260_bist.xpfm"
+FAMILY="zynqmp"
+PLATFORM="xilinx_zcu102_base_202420_1"
+SYSROOT="/home/tls/xilinx/sysroots/xilinx-zynqmp-common-v2024.2/sysroots/cortexa72-cortexa53-xilinx-linux"
 FREQ="300MHz"
 
 # App specific variables, change these if needed
@@ -76,29 +75,12 @@ echo "Compiling SW-HW version..."
 
 # Step 3.1: Compile the host
 echo "Compiling host code..."
-HOST_FLAGS="-O3 -DOFFLOAD -fno-lto"
-CPP_INCLUDES="-I$SYSROOT/usr/include/c++/13 \
-                -I$SYSROOT/usr/include/c++/13 \
-                -I$SYSROOT/usr/include/aarch64-linux-gnu/c++/13 \
-                -I$SYSROOT/usr/include/c++/13/backward \
-                -I$SYSROOT/usr/lib/gcc/aarch64-linux-gnu/13/include \
-                -I$SYSROOT/usr/local/include \
-                -I$SYSROOT/usr/include/aarch64-linux-gnu \
-                -I$SYSROOT/usr/include"
-XRT_INCLUDES="-I$SYSROOT/usr/include/xrt"
+ARM_INCLUDES="$SYSROOT/usr/include/"
+XRT_INCLUDES="$SYSROOT/usr/include/xrt"
+LIBS="$SYSROOT/lib"
+LINK="-lxrt++ -lxrt_core -lxrt_coreutil -lm"
 
-STARTUP_FILES="-B$SYSROOT/usr/lib/aarch64-linux-gnu \
-                -B$SYSROOT/usr/lib/gcc/aarch64-linux-gnu/13"
-
-LIBS_PATH="-L$SYSROOT/lib \
-            -L$SYSROOT/usr/lib \
-            -L$SYSROOT/usr/lib/aarch64-linux-gnu \
-            -L$SYSROOT/usr/local/lib \
-            -L$SYSROOT/usr/local/lib/aarch64-linux-gnu"
-LIBS_LINK="-lxrt++ -lxrt_core -lxrt_coreutil -lm -luuid"
-COMPILER_PATH=$SYSROOT/usr/libexec/gcc/aarch64-linux-gnu/13
-
-$ARM_COMP $HOST_CODE $BRIDGE_CODE --sysroot=$SYSROOT $HOST_FLAGS -o "${BUILD_DIR}/${APP_NAME}_host.elf" $CPP_INCLUDES $XRT_INCLUDES $STARTUP_FILES $LIBS_PATH $LIBS_LINK
+$ARM_COMP $HOST_CODE $BRIDGE_CODE --sysroot=$SYSROOT -O3 -o "${BUILD_DIR}/${APP_NAME}_host.elf" -DOFFLOAD -I$ARM_INCLUDES -I$XRT_INCLUDES -L$LIBS $LINK
 echo "Host code compiled successfully."
 
 # Step 3.2: Compile the kernel
@@ -114,7 +96,7 @@ echo "Kernel code compiled successfully."
 echo "Linking host and kernel..."
 mkdir -p ./kernel_linking
 cd ./kernel_linking
-v++ -l -t hw --platform $PLATFORM -o "../${BUILD_DIR}/${TOP_FUNCTION}.xclbin" "../${BUILD_DIR}/${TOP_FUNCTION}.xo" --temp_dir ./link
+v++ -l -t hw --platform $PLATFORM -o "../${BUILD_DIR}/${TOP_FUNCTION}.xclbin" "../${BUILD_DIR}/${TOP_FUNCTION}.xo" --temp_dir link
 mv ../${BUILD_DIR}/${TOP_FUNCTION}.xclbin.link_summary .
 mv ../${BUILD_DIR}/${TOP_FUNCTION}.xclbin.info .
 rm ../${BUILD_DIR}/${TOP_FUNCTION}.xo
