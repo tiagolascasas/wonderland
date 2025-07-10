ea8cf837e9ac40d7e8720fb38fac6bb63757eb93
tiagolascasas <tiagolascasas@outlook.com>
2025-07-10 18:54:26 +0100

    Big refactoring of perf2dotfile

--- PerfTools/README.md
+++ PerfTools/README.md
@@ -10,9 +10,11 @@ perf report --stdio --sort symbol --percent-limit 0 > full_callgraph.txt
 
 See "full_callgraph.txt" file for an example
 
-#### perf2dotfile \<input> \<output>
+#### perf2dotfile \<input> \<output> -f
 
 Converts a call graph obtained from perf into a Dotfile, ready to be rendered by Graphviz
+
+`-f` filters out calls to kernel functions and unnamed functions
 
 The call graph format should be folded, obtained, e.g., with the command:
 
--- PerfTools/perf2dotfile.py
+++ PerfTools/perf2dotfile.py
@@ -1,47 +1,100 @@
 #!/usr/bin/env python3
-import sys
 import re
+import argparse
 
-input = "perf-flat.txt" if len(sys.argv) < 2 else sys.argv[1]
-output = input.replace(".txt", ".dot") if len(sys.argv) < 3 else sys.argv[2]
 
-valid_line = re.compile(r"^(\d{1,3}(?:\.\d{1,2}))% ([_\w]+[_\w\W;]*)")
+def main():
+    args = parse_args()
 
-functions = {}
-edges = []
+    input = args.input
+    output = args.output
+    filter_kernel = args.filter
+    print(f"Input: {input}, Output: {output}, Filter: {filter_kernel}")
 
-with open(input, "r") as file:
-    for line in file:
-        line = line.strip()
-        match = valid_line.match(line)
-        if match:
-            percentage, fun_chain = match.groups()
-            discrete_funs = fun_chain.split(";")
-            n_percent = float(percentage)
+    functions, edges = build_graph(input, filter_kernel)
+    generate_dotfile(functions, edges, output)
 
-            leaf = discrete_funs[-1]
-            if leaf not in functions:
-                functions[leaf] = n_percent
-            else:
-                functions[leaf] += n_percent
 
-            pairs = [
-                [discrete_funs[i], discrete_funs[i + 1]]
-                for i in range(len(discrete_funs) - 1)
-            ]
-            for pair in pairs:
-                if edges.count((pair[0], pair[1])) == 0:
-                    edges.append((pair[0], pair[1]))
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Convert perf call graph to Dotfile format."
+    )
+    parser.add_argument(
+        "input",
+        type=str,
+        nargs="?",
+        default="folded_callgraph.txt",
+        help="Input file containing the perf call graph.",
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        nargs="?",
+        default="folded_callgraph.dot",
+        help="Output file for the Dotfile.",
+    )
+    parser.add_argument(
+        "-f",
+        "--filter",
+        action="store_true",
+        help="Filter out calls to kernel functions and unnamed functions.",
+    )
+    args = parser.parse_args()
+    return args
 
-with open(output, "w") as file:
-    file.write("digraph G {\n")
-    file.write("  rankdir=LR;\n")
-    file.write("  node [shape=box];\n")
 
-    for fun, percent in functions.items():
-        file.write(f'  "{fun}" [label="{fun} ({percent:.2f}%)"];\n')
+def is_kernel_function(name):
+    return name.startswith(("__", "_IO", "0x"))
 
-    for edge in edges:
-        file.write(f'  "{edge[0]}" -> "{edge[1]}";\n')
 
-    file.write("}\n")
+def build_graph(input, filter_kernel):
+    functions = {}
+    edges = []
+
+    valid_line = re.compile(r"^(\d{1,3}(?:\.\d{1,2}))% ([_\w]+[_\w\W;]*)")
+    with open(input, "r") as file:
+        for line in file:
+            line = line.strip()
+            match = valid_line.match(line)
+            if match:
+                percentage, fun_chain = match.groups()
+                discrete_funs = fun_chain.split(";")
+                n_percent = float(percentage)
+
+                leaf = discrete_funs[-1]
+                if leaf not in functions:
+                    functions[leaf] = n_percent
+                else:
+                    functions[leaf] += n_percent
+
+                pairs = [
+                    [discrete_funs[i], discrete_funs[i + 1]]
+                    for i in range(len(discrete_funs) - 1)
+                ]
+                for pair in pairs:
+                    if filter_kernel:
+                        if is_kernel_function(pair[0]):
+                            continue
+
+                    if edges.count((pair[0], pair[1])) == 0:
+                        edges.append((pair[0], pair[1]))
+    return functions, edges
+
+
+def generate_dotfile(functions, edges, output):
+    with open(output, "w") as file:
+        file.write("digraph G {\n")
+        file.write("  rankdir=LR;\n")
+        file.write("  node [shape=box];\n")
+
+        for fun, percent in functions.items():
+            file.write(f'  "{fun}" [label="{fun} ({percent:.2f}%)"];\n')
+
+        for edge in edges:
+            file.write(f'  "{edge[0]}" -> "{edge[1]}";\n')
+
+        file.write("}\n")
+
+
+if __name__ == "__main__":
+    main()
